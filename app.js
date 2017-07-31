497e51a04ef143318a66a9238990c0e204123d45
Manuel Hernandez <manuel@Elrond.fios-router.home>
2017-07-31 06:22:57 -0700

    s tart messaging

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var env = process.env.NODE_ENV || 'development';
 app.locals.ENV = env;
 app.locals.ENV_DEVELOPMENT = env == 'development';
 
-app.use(favicon(__dirname + '/client/content/img/favicon.ico'));
+//app.use(favicon(__dirname + '/client/content/img/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
